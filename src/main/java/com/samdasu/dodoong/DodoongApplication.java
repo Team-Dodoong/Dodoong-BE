d182a82e59ab1b107a05ba1e1b59cfca72b8ccde
nomellc <jyj100743@ewha.ac.kr>
2026-06-30 23:39:04 +0900

    chore: 초기 환경세팅

---
+++ src/main/java/com/samdasu/dodoong/DodoongApplication.java
@@ -0,0 +1,13 @@
+package com.samdasu.dodoong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DodoongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DodoongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samdasu/dodoong/DodoongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samdasu.dodoong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DodoongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
