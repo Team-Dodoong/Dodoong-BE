dec0a8c849cb121b3929a0b5ee297e96fb718b83
nomellc <jyj100743@ewha.ac.kr>
2026-07-10 00:54:38 +0900

    test: Testcontainers 설정 추가

--- src/test/java/com/samdasu/dodoong/DodoongApplicationTests.java
+++ src/test/java/com/samdasu/dodoong/DodoongApplicationTests.java
@@ -2,9 +2,22 @@ package com.samdasu.dodoong;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.TestPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @SpringBootTest
+@TestPropertySource(properties = {
+		"spring.jpa.hibernate.ddl-auto=create-drop",
+		"spring.jpa.show-sql=true"
+})
 class DodoongApplicationTests {
+	@Container
+	@ServiceConnection
+	static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.4");
 
 	@Test
 	void contextLoads() {
