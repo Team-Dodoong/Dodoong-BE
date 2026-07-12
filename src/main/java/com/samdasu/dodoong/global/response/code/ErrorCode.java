f9ef4ccd974de3b164a46515d72f3ceae51c2e88
nomellc <jyj100743@ewha.ac.kr>
2026-07-12 20:45:38 +0900

    feat: Member 에러 코드 추가

--- src/main/java/com/samdasu/dodoong/global/response/code/ErrorCode.java
+++ src/main/java/com/samdasu/dodoong/global/response/code/ErrorCode.java
@@ -27,11 +27,15 @@ public enum ErrorCode implements BaseCode {
     // 500 INTERNAL SERVER ERROR
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부에서 문제가 발생하였습니다."),
 
-    //Auth
+    // Auth
     // 400 BAD REQUEST
     DUPLICATE_LOGIN_ID(HttpStatus.BAD_REQUEST, "이미 사용 중인 아이디입니다."),
     // 401 UNAUTHORIZED
-    INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED,"아이디 또는 비밀번호가 올바르지 않습니다.");
+    INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED,"아이디 또는 비밀번호가 올바르지 않습니다."),
+
+    // Member
+    // 404 NOT FOUND
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하는 사용자가 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
