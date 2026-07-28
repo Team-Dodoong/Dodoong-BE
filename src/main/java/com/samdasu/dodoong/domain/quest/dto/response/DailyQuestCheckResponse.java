cdc74d99f0ab1175de187eab499cf71c19daf0ad
nomellc <jyj100743@ewha.ac.kr>
2026-07-28 11:51:39 +0900

    feat: 일일퀘스트 달성 dto 작성

--- src/main/java/com/samdasu/dodoong/domain/quest/dto/response/DailyQuestCheckResponse.java
+++ src/main/java/com/samdasu/dodoong/domain/quest/dto/response/DailyQuestCheckResponse.java
@@ -1,8 +1,19 @@
 package com.samdasu.dodoong.domain.quest.dto.response;
 
+import com.samdasu.dodoong.domain.quest.entity.DailyQuest;
+
 public record DailyQuestCheckResponse(
         DailyQuestSummary quest,
         int experience,
         int experienceDelta
 ) {
+    public static DailyQuestCheckResponse of(DailyQuest quest,
+                                             int experience,
+                                             int experienceDelta) {
+        return new DailyQuestCheckResponse(
+                DailyQuestSummary.from(quest),
+                experience,
+                experienceDelta
+        );
+    }
 }
