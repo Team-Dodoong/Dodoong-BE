1b507f4298170fa8d3d6d66dbb7d5adf357aa27e
nomellc <jyj100743@ewha.ac.kr>
2026-08-02 11:32:29 +0900

    feat: WebSocket 배포 설정 추가

--- src/main/java/com/samdasu/dodoong/global/config/WebSocketConfig.java
+++ src/main/java/com/samdasu/dodoong/global/config/WebSocketConfig.java
@@ -5,14 +5,12 @@ import com.samdasu.dodoong.global.websocket.WebSocketHandshakeHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -34,7 +32,18 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic"); // 서버 -> 클라이언트
+        registry.enableSimpleBroker("/topic")
+                .setHeartbeatValue(new long[]{10000, 10000})
+                .setTaskScheduler(webSocketHeartbeatScheduler()); // 서버 -> 클라이언트
         registry.setApplicationDestinationPrefixes("/app"); // 클라이언트 -> 서버
+    }
+
+    @Bean
+    public TaskScheduler webSocketHeartbeatScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(1);
+        scheduler.setThreadNamePrefix("ws-heartbeat-");
+        scheduler.initialize();
+        return scheduler;
     }
 }
