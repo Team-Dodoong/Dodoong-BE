4eb8e28428290230b2071fda6a8c447b314d9bf0
wys0530 <youn.soo.woo@gmail.com>
2026-08-02 14:45:33 +0900

    fix: 오타 수정

--- src/main/java/com/samdasu/dodoong/global/response/code/ErrorCode.java
+++ src/main/java/com/samdasu/dodoong/global/response/code/ErrorCode.java
@@ -91,7 +91,7 @@ public enum ErrorCode implements BaseCode {
     // S3
     // 400 BAD REQUEST
     UNSUPPORTED_IMAGE_TYPE(HttpStatus.BAD_REQUEST, "지원하지 않는 이미지 형식입니다."),
-    INVALID_PROFILE_IMAGE_KEY(HttpStatus.BAD_REQUEST,"유효하지 않은 프로필 이미지 key입니다.");
+    INVALID_PROFILE_IMAGE_KEY(HttpStatus.BAD_REQUEST,"유효하지 않은 프로필 이미지 key입니다."),
     // 500 INTERNAL SERVER ERROR
     FILE_UPLOAD_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "파일 업로드에 실패했습니다.");
 
